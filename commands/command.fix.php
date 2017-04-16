feaf1258e7110474942847a15ee8ac100eaae4cb
Marc <markimarc@users.noreply.github.com>
2017-04-16 22:44:55 +0200

    Revert "Overzicht update"

--- commands/command.fix.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Niels
- * Date: 4-1-2017
- * Time: 10:03
- */
-
-$telegram->sendMessage(array('chat_id' => CHAT_ID, 'text' => "Fix"));
--- commands/command.start.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Niels
- * Date: 4-1-2017
- * Time: 09:56
- */
-
-$telegram->sendMessage(array('chat_id' => CHAT_ID, 'text' => "Hoi. \nWil je weten wat ik allemaal kan? doe dan /commands"));
